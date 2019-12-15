fc2fe017ca20992547efb655fc1147fce8357de5
Vasily Gusev <vasily.guseff@gmail.com>
2019-12-15 14:56:43 +0300

    refactor: implement new tests

--- fancy-weather/test/test.js
+++ fancy-weather/test/test.js
@@ -2,6 +2,8 @@ import getApparent from '../src/js/logic/apparent.js';
 import getFahrenheit from '../src/js/logic/fahrenheit';
 import createBkg from '../src/js/controls/bkg';
 import createCity from '../src/js/city/city';
+import createDateTime from '../src/js/city/date-time';
+import { getStorage, setStorage } from '../src/js/storage';
 
 describe('calculates an apparent air temperature when wind 3 m/s', () => {
   it('0*C and 50% humidity', () => {
@@ -42,5 +44,50 @@ describe('return DOM element for City Name Div', () => {
   });
   it('return DOM element with id="city"', () => {
     expect(createCity().getAttribute('id')).toEqual('city');
+  });
+});
+
+describe('return DOM element for Date and Time', () => {
+  it('return DOM element', () => {
+    expect(createDateTime()).toBeDefined();
+  });
+  it('return DOM element', () => {
+    expect(createDateTime()).toBeInstanceOf(HTMLDivElement);
+  });
+  it('return DOM element with id="city"', () => {
+    expect(createDateTime().getAttribute('id')).toEqual('date-time');
+  });
+});
+
+describe('set and return items to/from app storage', () => {
+  it('set and return "lang" "en"', () => {
+    setStorage('lang', 'en');
+    expect(getStorage('lang')).not.toEqual('be');
+    expect(getStorage('lang')).not.toEqual('ru');
+    expect(getStorage('lang')).toEqual('en');
+  });
+  it('set and return "lang" "ru"', () => {
+    setStorage('lang', 'ru');
+    expect(getStorage('lang')).not.toEqual('be');
+    expect(getStorage('lang')).not.toEqual('en');
+    expect(getStorage('lang')).toEqual('ru');
+  });
+  it('set and return "lang" "be"', () => {
+    setStorage('lang', 'be');
+    expect(getStorage('lang')).not.toEqual('en');
+    expect(getStorage('lang')).not.toEqual('ru');
+    expect(getStorage('lang')).toEqual('be');
+  });
+  it('set and return "degrees" "c"', () => {
+    setStorage('degrees', 'c');
+    expect(getStorage('degrees')).not.toEqual('f');
+    expect(getStorage('degrees')).not.toEqual(/!c/);
+    expect(getStorage('degrees')).toEqual('c');
+  });
+  it('set and return "degrees" "f"', () => {
+    setStorage('degrees', 'f');
+    expect(getStorage('degrees')).not.toEqual('c');
+    expect(getStorage('degrees')).not.toEqual('/!f/');
+    expect(getStorage('degrees')).toEqual('f');
   });
 });
